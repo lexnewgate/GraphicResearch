e63d9d5cdde5de92ade5387aa77bd4715da1a390
lexnewgate <lexnewgate@gmail.com>
2019-04-19 17:56:33 +0800

    opentk solution setup

--- OpentkProjects/OpenTkTest/Properties/AssemblyInfo.cs
+++ OpentkProjects/OpenTkTest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed037a44-74f8-4aca-87d4-f0b199fba6e1")]
+[assembly: Guid("dbcacbc1-db15-4648-a272-7b535d2762c2")]
 
 // Version information for an assembly consists of the following four values:
 //
