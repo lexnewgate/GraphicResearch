3b1cbb35037cfb77cfe3cfd57b5fa624a52c8eee
lexnewgate <lexnewgate@gmail.com>
2019-04-23 12:58:57 +0800

    bak

---
+++ OpentkProjects/OpenTkTest/Scripts/Texture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace OpenTkTest.Scripts
+{
+    class Texture
+    {
+        private int Handle;
+
+        public Texture(string path)
+        {
+            Handle = GL.GenTexture();
+
+            Image<Rgba32> image = Image.Load(path);
+        }
+
+        public void Use()
+        {
+            GL.BindTexture(TextureTarget.Texture2D,Handle);
+        }
+
+
+    }
+}
