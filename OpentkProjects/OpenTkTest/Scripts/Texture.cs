35ec62a7720681c4de2e740e161a433be5e36bc2
lexnewgate <lexnewgate@gmail.com>
2019-04-24 19:52:42 +0800

    Update Texture.cs

--- OpentkProjects/OpenTkTest/Scripts/Texture.cs
+++ OpentkProjects/OpenTkTest/Scripts/Texture.cs
@@ -18,6 +18,16 @@ namespace OpenTkTest.Scripts
             Handle = GL.GenTexture();
 
             Image<Rgba32> image = Image.Load(path);
+            image.Mutate(x=>x.Flip(FlipMode.Vertical));
+            Rgba32[] tempPixels = image.GetPixelSpan().ToArray();
+            List<byte>pixels=new List<byte>();
+            foreach (var tempPixel in tempPixels)
+            {
+                pixels.Add(tempPixel.R);
+                pixels.Add(tempPixel.G);
+                pixels.Add(tempPixel.B);
+                pixels.Add(tempPixel.A);
+            }
         }
 
         public void Use()
