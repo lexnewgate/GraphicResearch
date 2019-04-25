ef9dea743b68d01ee4c8858e61e8cf2c6f4dffdb
lexnewgate <lexnewgate@gmail.com>
2019-04-25 19:05:05 +0800

    fix localscale

--- UnityTinyRenderer/Assets/TinyRenderer.cs
+++ UnityTinyRenderer/Assets/TinyRenderer.cs
@@ -16,6 +16,8 @@ public class TinyRenderer : MonoBehaviour
         var layout = canvas.GetComponent<GridLayoutGroup>();
         layout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
         layout.constraintCount = width;
+        var canvasScaler = canvas.GetComponent<CanvasScaler>();
+        layout.cellSize=new Vector2(canvasScaler.referenceResolution.x/width,canvasScaler.referenceResolution.y/height);
 
         //init pixels
         for (int i = 0; i < width * height; i++)
@@ -23,6 +25,7 @@ public class TinyRenderer : MonoBehaviour
             var pixelGo = Object.Instantiate<GameObject>(point);
             var pixel = pixelGo.GetComponent<Image>();
             pixel.transform.SetParent(canvas.transform);
+            pixel.transform.localScale = Vector3.one;
             pixel.color = new Color(Random.Range(0f,1f), Random.Range(0f,1f), 
                 Random.Range(0f,1f), Random.Range(0f,1f));
         }
