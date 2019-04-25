2160a76319bd4cc88be47bbd12dd8be815119226
lexnewgate <lexnewgate@gmail.com>
2019-04-25 18:27:17 +0800

    update

---
+++ UnityTinyRenderer/Assets/TinyRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinyRenderer : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public GameObject point;
+    public GameObject canvas;
+
+    void Start()
+    {
+        for (int i = 0; i < width*height; i++)
+        {
+            Object.Instantiate<GameObject>(point).transform.SetParent(canvas.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
