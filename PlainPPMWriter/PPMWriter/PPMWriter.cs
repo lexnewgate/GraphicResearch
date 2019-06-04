5cabdc1ef123a5a1852a083391419ae4dc9daa4b
lexnewgate <lexnewgate@gmail.com>
2019-06-04 17:52:22 +0800

    add ppm writer

---
+++ PlainPPMWriter/PPMWriter/PPMWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPMWriter
+{
+    class PPMWriter
+    {
+        //todo
+        //write comments #
+        // no line should longer than 70
+
+        //write header plain format is P3
+        private const string Header = "P3";
+
+        //max color value less than 65536
+        private const string MaxColor = "255";
+        private readonly int _width;
+        private readonly int _height;
+
+        private readonly string []_pixels;
+
+        public PPMWriter(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+            this._pixels=new string[width*height];
+        }
+
+        public void WritePixel(int x, int y, int r, int g, int b)
+        {
+            int index = x + this._width * (_height-y-1);
+            _pixels[index] = $"{r} {g} {b}";
+        }
+
+        public void Output(string fp)
+        {
+            var ppmContents = new List<string> {Header, $"{_width} {_height}", MaxColor};
+            ppmContents.AddRange(_pixels);
+            if (Path.GetExtension(fp) != "ppm")
+            {
+                fp = $"{fp}.ppm";
+            }
+            File.WriteAllLines(fp, ppmContents);
+        }
+    }
+}
--- PlainPPMWriter/PPMWriter/Program.cs
+++ PlainPPMWriter/PPMWriter/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//ref: http://netpbm.sourceforge.net/doc/ppm.html
+
 namespace PPMWriter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var ppmWriter=new PPMWriter(2,2);
+            ppmWriter.WritePixel(0,0,255,0,0);
+            ppmWriter.WritePixel(1,0,0,255,0);
+            ppmWriter.WritePixel(0,1,0,0,255);
+            ppmWriter.WritePixel(1,1,0,0,0);
+            ppmWriter.Output("2");
         }
     }
 }
